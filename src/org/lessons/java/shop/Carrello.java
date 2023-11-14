09e4f0a121245a0f39e0672087ab4ebc309a2db1
patrictobola <tobola92@gmail.com>
2023-11-14 15:30:08 +0100

    done carrello with new television

--- src/org/lessons/java/shop/Carrello.java
+++ src/org/lessons/java/shop/Carrello.java
@@ -5,16 +5,39 @@ import java.util.Scanner;
 public class Carrello {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		Prodotto[] carrello = new Prodotto[10];
 		
-		System.out.println("Seleziona un prodotto tra: \n"
-				+ "Smartphone \n"
-				+ "Televisore \n"
-				+ "Cuffie \n"
-				+ "");
-		String product = in.nextLine().toLowerCase();
-		if(product.equals("smartphone")) {
-			System.out.println("daje");
+		System.out.println("Vuooi inserire un nuovo elemento? Si/No: ");
+		String answer = in.next().toLowerCase();
+		
+		if (answer.equals("no")) {
+			System.out.println("TODO lista di oggetti nel carrello");
+		} else {
+			System.out.println("Quale tipo di prodotto vuoi aggiungere? Smartphone/Cuffia/TV");
+			String product = in.next().toLowerCase();
+			if(product.equals("tv")) {
+				carrello[0] = createTV(in);
+			}
+			System.out.println(carrello[0]);
 		}
 		
+		
 	}
+	private static Televisore createTV(Scanner in) {
+		System.out.println("Inserisci nome");
+		String nome = in.next();
+		System.out.println("Inserisci breve descrizione");
+		String descrizione = in.next();
+		System.out.println("Inserisci il prezzo lordo");
+		float prezzo = in.nextFloat();
+		System.out.println("Inserisci l'iva del prodotto");
+		int iva = in.nextInt();
+		System.out.println("Inserisci la grandezza della tv espressa in pollici");
+		int inch = in.nextInt();
+		System.out.println("La tv è smart? Rispondi: true/false");
+		boolean smart = in.nextBoolean();
+		
+		return new Televisore(nome, descrizione, prezzo, iva, inch, smart);
+	}
+	
 }
